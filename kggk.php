dfd5491c76ba3d44b995e1e9a8623542826bacd9
Wxxxaa <108240070+Wxxxaa@users.noreply.github.com>
2022-06-27 22:43:45 +0300

    Add files via upload

---
+++ kggk.php
@@ -0,0 +1,22 @@
+<?php
+
+esho "first tutorial";
+
+$TOKEN = "5498064127:AAHDNnE-NpJEB2SJQGbc4MTBK1AXxUuRu2Q";
+
+
+ requier "function.php";
+
+SetWebhook("https://github.com/Wxxxaa/sturdy-happiness.php.git");
+
+SetWebhook("git remote add origin https://github.com/Wxxxaa/sturdy-happiness.php.git
+ git Branch -M main 
+git push -u origin main main");
+
+SetWebhook ("");
+ 
+if ($text=="/start"):
+
+ SendMessage($chat_id,"Ahmedcco");
+
+endif;
